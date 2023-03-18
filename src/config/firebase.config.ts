c8da1c2435f979aece4c23cc49d47baa98f20722
Arijit <arijitdasofficial@gmail.com>
2023-03-18 23:26:46 +0530

    feat: initial

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdT6ENulQniandLkUhCvSY7yGBed5NOuE',
+  authDomain: 'fir-learning-e75e8.firebaseapp.com',
+  projectId: 'fir-learning-e75e8',
+  storageBucket: 'fir-learning-e75e8.appspot.com',
+  messagingSenderId: '515342970121',
+  appId: '1:515342970121:web:558e7e72819d1bd7c977d8',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
