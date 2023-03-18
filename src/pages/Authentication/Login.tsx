8dddf8026fe19519a8e0e33b844561181e61b0b9
Arijit <arijitdasofficial@gmail.com>
2023-03-19 05:17:44 +0530

    feat: initial

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+interface Login {
+  email: string;
+  password: string;
+}
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email().required('Email is required'),
+  password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters'),
+});
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: 'onChange',
+    resolver: yupResolver(loginSchema),
+  });
+
+  async function onSubmit(data: Login) {
+    console.log(data);
+  }
+
   return (
     <>
       <section className="h-[calc(100vh-60px)] bg-gray-50 dark:bg-gray-900 flex justify-center items-center">
@@ -11,20 +38,21 @@ export default function Login() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6">
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Your email
                   </label>
                   <input
                     type="email"
-                    name="email"
                     id="email"
+                    {...register('email')}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 
                       focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
                     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="sample@example.com"
                   />
+                  {errors?.email && <p className=" mt-2 text-sm text-red-400">{errors.email.message}</p>}
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -32,13 +60,14 @@ export default function Login() {
                   </label>
                   <input
                     type="password"
-                    name="password"
                     id="password"
+                    {...register('password')}
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 
                       focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
                     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
+                  {errors?.password && <p className=" mt-2 text-sm text-red-400">{errors.password.message}</p>}
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
